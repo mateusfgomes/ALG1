439ab0332daa67ab28a3695dc5ce3287848d4781
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2018-09-19 15:30:09 -0300

    Update site.h

--- site.h
+++ site.h
@@ -5,11 +5,11 @@ typedef struct site SITE; //struct de um site
 
 SITE* create_site(void); //funcao que cria o site
 
-void prepare_site(FILE* fp, SITE* s, int i, int j); //funcao que prepara um site para ser acrescentado na lista
+void prepare_site(FILE* fp, SITE* s, int i, int j, int list_size); //funcao que prepara um site para ser acrescentado na lista
 
 void next_site(SITE* a, SITE* aux); //funcao que cria um novo site para encadear a lista
 
-void print_site(SITE* site); //funcao recursiva que printa os dados de um site
+void print_site(SITE* site, int* h); //funcao recursiva que printa os dados de um site
 
 
 #endif
