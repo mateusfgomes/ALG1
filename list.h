207c6d42c24134644475b0757c0c456041a8a993
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2018-09-19 22:36:29 -0300

    Update list.h

--- list.h
+++ list.h
@@ -4,11 +4,17 @@
 #include "site.h"
 
 typedef struct list LIST; //struct da lista de sites
+typedef struct node NODE;
 
 LIST* create_list(void); //funcao que cria a lista para armazenar os sites
-
+void delete_list(LIST *L);
+void delete_node(NODE *N);
 LIST* scan_file(FILE* fp, int n_lines); //funcao que escaneia o arquivo, recebe o arquivo e o numero de linhas dele
-
-void print_list(LIST* l); //funcao que printa a lista de sites
+int list_insertion(LIST *L, SITE *S);
+int empty_list(LIST *L);
+int list_size(LIST *L);
+void print_list(LIST *L);
+int list_remove(LIST *L, int code);
+SITE *list_search(LIST *L, int code);
 
 #endif
