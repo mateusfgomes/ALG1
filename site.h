498e8d910a212daf8c21500eb52f06599602f0fb
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2018-11-15 21:29:31 -0200

    Update site.h

--- site.h
+++ site.h
@@ -15,5 +15,8 @@ int new_keyword(SITE *S);
 int change_relevance(SITE *S, int relevance);
 void save_site(FILE *fp, SITE *S);
 SITE* keyword_found(SITE* root, char search[51]);
+char* keyword_to_search(char** keywords, int word);
+int site_nkey(SITE* s);
+char** site_keywords(SITE* s);
 
 #endif
