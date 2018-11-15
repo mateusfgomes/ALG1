c742cf9f7eadf1e4929275cbccbfc86834e3c3ae
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2018-11-15 15:31:41 -0200

    Update site.h

--- site.h
+++ site.h
@@ -1,16 +1,18 @@
 #ifndef SITE_H
 #define SITE_H
+#include <stdio.h>
 
 typedef struct site SITE;
 
 SITE* create_site(void);
 SITE *read_file_sites(FILE *fp);
-void print_site(SITE *S);
-SITE *read_new_site(int code);
+void print_site(SITE *S, int type);
+SITE *read_new_site(int code, int relevance);
 int site_code(SITE *S);
+int site_key(SITE *S);
 int new_keyword(SITE *S);
-int change_relevance(SITE *S);
+int change_relevance(SITE *S, int relevance);
 void save_site(FILE *fp, SITE *S);
-int check_code(int* code);
+SITE* keyword_found(SITE* root, char search[51]);
 
 #endif
