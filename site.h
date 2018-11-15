488a453d274915f33ab416e0cd64964951596778
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2018-11-15 17:06:50 -0200

    Update site.h

--- site.h
+++ site.h
@@ -6,9 +6,10 @@ typedef struct site SITE;
 
 SITE* create_site(void);
 SITE *read_file_sites(FILE *fp);
-void print_site(SITE *S, int type);
+void print_site(SITE *S);
 SITE *read_new_site(int code, int relevance);
 int site_code(SITE *S);
+int site_relevance(SITE *S);
 int site_key(SITE *S);
 int new_keyword(SITE *S);
 int change_relevance(SITE *S, int relevance);
