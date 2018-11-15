40aa4a653a87c942db87430504e39e69e35d116e
Mateus Ferreira <39449176+mateusfgomes@users.noreply.github.com>
2018-11-15 17:07:33 -0200

    Update avl.h

--- avl.h
+++ avl.h
@@ -1,13 +1,15 @@
 #ifndef AVL_H
 #define AVL_H
 #include "site.h"
+#include "list.h"
 
 #define max(a, b) ((a > b) ? a : b)
 
 typedef struct avl AVL;
 typedef struct node NODE;
 
-AVL* scan_file(FILE* fp, int n_lines);
+void delete_node_avl(NODE* a);
+AVL* scan_file_avl(FILE* fp, int n_lines);
 AVL *avl_create(void);
 void delete_avl_aux(NODE *root);
 void avl_delete(AVL **tree);
@@ -20,12 +22,13 @@ NODE *rotate_right_left(NODE *a);
 NODE *avl_insert_node(NODE *root, SITE *site);
 int avl_insert(AVL *tree, SITE *site);
 int avl_remove(AVL* A, int code);
-int code_found(AVL* L, int code);
+int code_found_avl(AVL* L, int code);
 NODE* avl_search_code(NODE* root, int code);
 SITE* avl_search(NODE* root, int code);
 NODE* avl_root(AVL* T);
 void avl_print(AVL* A);
 int avl_search_keyword(AVL* A, char search[51]);
-void tree_search(NODE* root, char search[51], int* flag);
+void tree_search(NODE* root, char search[51], int* flag, LIST* L);
+
 
 #endif
